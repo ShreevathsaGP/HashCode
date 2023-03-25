95cf5d16d58b5fe0864623f86be62b40742b8d38
ShreevathsaGP <gpshreevathsa@gmail.com>
2023-03-26 00:06:54 +0530

    prophet pipeling begun

---
+++ cumulate_prophets.py
@@ -0,0 +1,19 @@
+import json
+import os
+import prophet
+from prophet.serialize import model_to_json, model_from_json
+
+models = list(filter(lambda x: x.split('.')[-1] == "json", os.listdir("data/prophet_models")))
+
+prophets = {}
+path = "data/prophet_models/"
+for m in models:
+    with open(path + m, 'r') as f:
+        assert len(m.split('.')) == 2, "name does not match <name>.json"
+        
+        print(m.split(".")[0])
+        prophets[m.split(".")[0]] = json.load(f)
+
+
+with open("prophets.json", 'w') as f:
+    json.dump(prophets, f, indent = 4)
